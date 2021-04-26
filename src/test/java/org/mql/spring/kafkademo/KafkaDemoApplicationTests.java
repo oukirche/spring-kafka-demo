a1423dfa10b46909b58d16ff0b955e10bc1f81a5
Abdessamade OUKIRCHE <aoukirche@sii.ma>
2021-04-26 09:29:43 +0000

    initi demo for apache kafka with spring

---
+++ src/test/java/org/mql/spring/kafkademo/KafkaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mql.spring.kafkademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
